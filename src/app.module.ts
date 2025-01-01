2033c48621a0fd3b4292b654d3cb3b17a9dc2d9c
utan323 <nglephuc03@gmail.com>
2025-01-01 23:09:39 +0700

    hotfix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommonsModule } from 'libs/common/src';
+import { CommonsModule } from '@libs/common';
 
 @Module({
   imports: [CommonsModule],
