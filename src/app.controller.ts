99286bb64cabda0bec62fe5c0fb42f723908d112
utan323 <nglephuc03@gmail.com>
2025-01-01 23:15:29 +0700

    hotfix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-//import { CommonsService } from '@libs/common/src';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    //private readonly commonsService: CommonsService,
   ) {}
 
   @Get()
